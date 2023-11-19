b8e115283e16ce973d1ab460de7a066bd6a82577
darylkell <46514397+darylkell@users.noreply.github.com>
2023-11-19 12:59:18 +0800

    Add -newer arg

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 '''
 
 import argparse
+import re
 import sys
+
 from pathlib import Path
 
 from query import Query
@@ -25,6 +27,11 @@ def parse_arguments():
 	# Optional argument for download location
 	parser.add_argument('-output', '--o', dest='download_path', type=str, default='./',
 		help='Destination directory (default: "./")'
+	)
+
+	# Optional argument for newer issues
+	parser.add_argument('-newer', '--n', dest="newer", type=int, default=False,
+		help='Search for issues including newer ones, requires an integer in the search (default: False)'
 	)
 
 	# Optional argument for the number of results
@@ -56,22 +63,33 @@ def parse_arguments():
 def main():
 	args = parse_arguments()
 
-	query = Query(args.query, args.results, args.verbose, args.download_path)
-	
-	query.find_pages()
-	if args.test:
-		print("\nPage links found:")
-		for i, (url, title) in enumerate(query.page_links.items(), start=1):
-			print(f"{i}) {title}: {url}")
-	
-	query.get_download_links()
-	if args.test:
-		print("\nComic links found:")	
-		for i, (url, title) in enumerate(query.comic_links.items(), start=1):
-			print(f"{i}) {title}: {url}")
-	
-	if not args.test:
-		query.download_comics()
+	try:
+		i = -1
+		while True: # loop used to continue searching for issues until one cannot be found, if args.newer set
+			if args.newer:
+				i += 1
+				query = Query(f"{args.query} {args.newer + i}", args.results, args.verbose, args.download_path)
+			else:
+				query = Query(args.query, args.results, args.verbose, args.download_path)
+			
+			query.find_pages()
+			query.get_download_links()
+
+			if args.test:
+				print("\nPage links found:")
+				for i, (url, title) in enumerate(query.page_links.items(), start=1):
+					print(f"{i}) {title}: {url}")
+			
+				print("\nComic links found:")	
+				for i, (url, title) in enumerate(query.comic_links.items(), start=1):
+					print(f"{i}) {title}: {url}")
+			else:
+				query.download_comics()
+
+			if not query.comic_links or not args.newer:
+				break
+	except KeyboardInterrupt:
+		sys.exit(1)
 
 
 if __name__ == "__main__":
